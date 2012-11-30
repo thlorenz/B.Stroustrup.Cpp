fbae754810051b8a1e3c5172abe25a65799f0bc5
Thorsten Lorenz <thlorenz@gmx.de>
2012-11-30 08:33:47 -0500

    half completed e03

---
+++ ch05/03.cc
@@ -0,0 +1,15 @@
+
+typedef unsigned char         uchar;
+typedef const unsigned char   const_uchar;
+typedef int*                  pint;
+typedef char**                ppchar;
+
+// pointer to arrays of char
+typedef char*                 pchar[];
+
+// array of 7 pointers to int
+typedef int*                  spint[7];
+
+// pointer to an array of 7 pointers to int
+
+// array of 8 arrays of 7 pointers to int
