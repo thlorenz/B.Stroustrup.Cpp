18a684b4d57f12d7766d4d0edb50aa3afa5a0989
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-01 12:21:52 -0500

    completed ch05/03

--- ch05/03.cc
+++ ch05/03.cc
@@ -1,15 +1,17 @@
 
 typedef unsigned char         uchar;
 typedef const unsigned char   const_uchar;
-typedef int*                  pint;
-typedef char**                ppchar;
+typedef int                   *pint;
+typedef char                  **ppchar;
 
-// pointer to arrays of char
+// pointer to array of char
 typedef char                 (*pchar)[];
 
 // array of 7 pointers to int
-typedef int*                  spint[7];
+typedef int                  *spint[7];
 
 // pointer to an array of 7 pointers to int
+typedef int                  *(*pspint)[7];
 
 // array of 8 arrays of 7 pointers to int
+typedef int                  *a8a7pint[8][7];
