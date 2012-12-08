9f2f054b420d42cbb3b673a080252e318ab98508
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-08 17:55:45 -0500

    6/16 complete

--- ch06/16.cc
+++ ch06/16.cc
@@ -6,13 +6,26 @@ using namespace std;
 
 int atoi_(const char *s)
 {
-  int len = strlen(s);
-  int mul = pow(10, len);
-  int sign = 1;
-  int val = 0;
+  // Assumes input is a string of a valid number (i.e. no extra characters contained)
 
-  if (*s == '-') { sign = -1; mul /= 10; *s++; }
-  while (*s) val += (*s++ - '0') * (mul /= 10);
+  int base =  10;
+  int sign =  1;
+  int val  =  0;
+
+  if (*s == '-') { sign = -1; s++; }
+
+  if (*s == '0' && *(s + 1) == 'x') {
+    base = 16; s += 2;
+  }
+  else if (*s == '0' && *(s + 1)) {
+    base = 8; s++;
+  }
+
+  while (*s) {
+    char c = toupper(*s++);
+    val *= base;
+    val += isalpha(c) ? (c - 'A' + 10) : (c - '0');
+  }
 
   return val * sign;
 }
@@ -23,8 +36,8 @@ void assert_atoi_(pair<const char*, int> pair)
   int expected = pair.second;
   int actual;
 
-  cout << "atoi(" << s << ") == " << expected << " actual: " << actual;
   actual = atoi_(s);
+  cout << "atoi(" << s << ") == " << expected << " actual: " << actual;
   assert(actual == expected);
   cout << " OK." << endl;
 }
@@ -43,8 +56,36 @@ void test_decimals()
   for_each(decimal_notation, decimal_notation + cases, assert_atoi_);
 }
 
+void test_octals()
+{
+  const pair<const char*, int> octal_notation[] = {
+    make_pair("01", 1),
+    make_pair("013", 11),
+    make_pair("-013", -11)
+  };
+  int cases = (sizeof(octal_notation) / sizeof(pair<const char*, int>));
+  for_each(octal_notation, octal_notation + cases, assert_atoi_);
+}
+
+void test_hexadecimals()
+{
+  const pair<const char*, int> octal_notation[] = {
+    make_pair("0x0", 0),
+    make_pair("0x1", 1),
+    make_pair("0xA", 10),
+    make_pair("-0xA", -10),
+    make_pair("0xF", 15),
+    make_pair("0xFF", 255),
+    make_pair("-0xFF", -255)
+  };
+  int cases = (sizeof(octal_notation) / sizeof(pair<const char*, int>));
+  for_each(octal_notation, octal_notation + cases, assert_atoi_);
+}
+
 int main(int argc, const char *argv[])
 {
   test_decimals();
-  return 0;
+  test_octals();
+  test_hexadecimals();
+  cout << endl << "result: " << atoi_("0xF") << endl; return 0;
 }
